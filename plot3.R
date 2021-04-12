b581902781eb75a8bd339380666161c1613f07a8
josephinejcheung <josephinejcheung@gmail.com>
2021-04-12 14:45:07 +0100

    updated plot 4

--- plot3.R
+++ plot3.R
@@ -9,13 +9,12 @@ df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
 df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
 df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
 
+
 #Plot 3
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
-
 plot(df$Sub_metering_1, type= 'l', xaxt="n", xlab ="", ylab = "Energy sub metering")
 points(df$Sub_metering_2,type="l", col="red")
 points(df$Sub_metering_3,type="l", col="blue")
 axis(1,at = c(0,nrow(df)/2, nrow(df)), labels = c("Thu","Fri","Sat"))
 legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-
 dev.off()
--- plot4.R
+++ plot4.R
@@ -8,14 +8,29 @@ df$Date <- as.Date(df$Date, format = "%d/%m/%Y")
 df$Sub_metering_1 <- as.numeric(df$Sub_metering_1)
 df$Sub_metering_2 <- as.numeric(df$Sub_metering_2)
 df$Sub_metering_3 <- as.numeric(df$Sub_metering_3)
+df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
+df$Global_active_power <- as.numeric(df$Global_active_power)
+df$Voltage <- as.numeric(df$Voltage)
 
 #Plot 4
 png(filename = "plot4.png", width = 480, height = 480, units = "px")
+par(mfrow = c(2,2))
 
+#Top left
+plot(df$Global_active_power, type= 'l', xaxt="n", xlab ="", ylab = "Global Active Power (kilowatts)")
+axis(1,at = c(0,nrow(df)/2, nrow(df)), labels = c("Thu","Fri","Sat"))
+#Top right
+plot(df$Voltage, type= 'l', xaxt="n", xlab ="datetime", ylab = "Voltage")
+axis(1,at = c(0,nrow(df)/2, nrow(df)), labels = c("Thu","Fri","Sat"))
+
+#bottom left 
 plot(df$Sub_metering_1, type= 'l', xaxt="n", xlab ="", ylab = "Energy sub metering")
 points(df$Sub_metering_2,type="l", col="red")
 points(df$Sub_metering_3,type="l", col="blue")
 axis(1,at = c(0,nrow(df)/2, nrow(df)), labels = c("Thu","Fri","Sat"))
-legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), box.lwd=0,box.col = "transparent")
+#bottom right
+plot(df$Global_reactive_power, type= 'l', xaxt="n", xlab ="datetime", ylab = "Global Rective Power")
+axis(1,at = c(0,nrow(df)/2, nrow(df)), labels = c("Thu","Fri","Sat"))
 
 dev.off()
